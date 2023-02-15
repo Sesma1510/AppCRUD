aca48bf41295915a5a3c41c9cdc5f4dca29a26bf
KARLALOPEZWDFTJAN2023 <karlacarbajalguillots@gmail.com>
2023-02-15 09:59:27 -0600

    AVANCE

--- app.js
+++ app.js
@@ -30,9 +30,12 @@ app.use("/", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
-const profileRoutes = require("./routes/profile.routes");
-app.use("/profile", profileRoutes)
+const adminRoutes = require("./routes/admin.routes");
+app.use("/admin", adminRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
+
+//hola
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", { userInSession: req.session.currentUser });
 });
 
 module.exports = router;
-
