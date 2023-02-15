dac17937c8e7bd1527379eb3f4f6b626bcf7ae6f
Eduardo Sesma <sesma.2012@hotmail.com>
2023-02-15 11:18:32 -0600

    Avances en el dashboard

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/dashboard", (req, res, next) => {
+  res.render("admin/dashboard", { userInSession: req.session.currentUser });
+});
+
+router.get("/dashboard/server", (req, res, next) => {
+  res.render("admin/server", { userInSession: req.session.currentUser });
+});
+
 module.exports = router;
